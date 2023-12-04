910939a339ea07d72ca09c6926f1927916e6fad8
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2023-12-05 01:38:44 +0600

    userschema added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-// const express = require("express");
-const app: Application = express();
-//const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-app.use(express.json());
-app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const app: Application = express()
 
-export default app;
+app.use(express.json())
+app.use(cors())
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import app from "./app";
-import mongoose from "mongoose";
-import config from "./app/config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './app/config'
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
+    await mongoose.connect(config.database_url as string)
 
-main();
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main()
