246725c3357407eee211dad3034296ebe6868dad
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2023-12-05 23:27:58 +0600

    insert routes

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -4,4 +4,6 @@ import { userController } from './user.controller'
 const router = express.Router()
 
 //route will call  user controller
-router.post('create-user' userController.createUser)
+router.post('create-user', userController.createUser)
+
+export const userRoutes = router
